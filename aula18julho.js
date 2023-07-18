95f3d2a06ce618c9ff4d5dfc6185a2d079cd3bad
Ceciany <120055945+Ceciany@users.noreply.github.com>
2023-07-18 19:12:51 +0000

    idade

--- aula18julho.js
+++ aula18julho.js
@@ -1,9 +1,3 @@
-console.log("******** MENU *******")
-console.log("1 - Listar Clientes")
-console.log("2 - Cadastrar Clientes")
-console.log("3 - Editar Cliente")
-console.log("4 - Remover Cliente")
-console.log("0 - Sair do Sistema")
-console.log("*********************")
-console.log("Opção: __ ")
-console.log("*********************")
+var idade;
+idade=20;
+console.log(idade);
