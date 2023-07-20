bb7572b120addc4e4f9f25cc98c57e309ab64ec9
Ceciany <120055945+Ceciany@users.noreply.github.com>
2023-07-20 19:46:57 +0000

    if e else

---
+++ aula20julho.js
@@ -0,0 +1,41 @@
+var n=9;
+
+if (n>0){
+    console.log("o número é positivo")
+}
+    else{
+        console.log("o número é negativo")
+    }
+
+if (n%2==0){
+    console.log(" o número é par")
+}
+else{
+    console.log("o número é ímpar")
+}
+
+var exerc=("vazia")
+if (exerc==""){
+    console.log("a string é vazia")
+}
+else{
+    console.log(exerc)
+}
+
+
+if (n>10){
+    console.log("o número é maior que 10")
+}
+else{
+    console.log("o número é menor que 10. Sua multiplicação por 2 é " + n*2)
+}
+
+
+var fazSol=true
+if((typeof fazSol) == "boolean"){
+    console.log("é boleano");
+}
+else{
+    fazSol=false
+    console.log(fazSol)
+}
