590dfcb03b6a479f5fd312fde0857aae8c379622
Ceciany <120055945+Ceciany@users.noreply.github.com>
2023-08-31 18:13:31 +0000

    aula

---
+++ exercicios-31agosto.js
@@ -0,0 +1,19 @@
+// 01
+const readline = require ("readline-sync");
+// let nome = readline.question("Nome do aluno: ");
+// let nota1 = readline.questionFloat("Digite a primeira nota: ");
+// let nota2 = readline.questionFloat("Digite a segunda nota: ");
+// let nota3 = readline.questionFloat("Digite a terceira nota: ");
+// let media = (nota1+nota2+nota3)/3;
+
+// console.log(`A média do aluno ${nome}, é ${media}. `);
+
+// 02
+// let ano = readline.questionInt("Insira o ano: ");
+// if (ano%400==0 || ano%4==0 && ano%100!==0){
+//     console.log("O ano é bissexto.");
+// }
+// else {
+// console.log("O ano não é bissexto.");
+// }
+
